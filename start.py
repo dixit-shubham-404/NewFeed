09fb4c0877f6799242ad0c5923a73fa48a79d671
sdixit1998 <34706831+sdixit1998@users.noreply.github.com>
2020-03-03 23:27:58 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,21 +1,25 @@
 from media import *
+from research import *
 import os
 
-
-
-print("\t\tWelcome to your new feeds...!!")
+print("\t\tWelcome to new feeds!!")
+print('=============================================================================================')
 print("So today which section do you want to explore")
-print("1. Want some your favorite videos..?")
-print('2. Want to know what is happenning around the globe..?')
-print('3. Have some research to do..?')
-choice = int(input("\n> "))
+print("1. Want some your favorite vedios")
+print('2. Want to know what is happenning around the globe')
+print('3. Have some research to do')
+print('=============================================================================================')
+choice = int(input("So,  what do you want to do??\n"))
 
 if choice==1:
-    print('So get ready for youtube')
+    print('=============================================================================================')
     opeartion()
 elif choice==2:
+    print('=============================================================================================')
     print('Get ready for news')
+
 elif choice==3:
-    print('Get ready for research')
+    print('=============================================================================================')
+    do_research()
 else:
     print('Your Bad!! You entered something wrong!!')
